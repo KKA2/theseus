6a61403d1e578084b0252972541c38cfeb80cb2d
akeenan2 <ann.keenan2@gmail.com>
2016-03-19 15:22:36 -0400

    rename images and add dynamic functionality to theseus.js

--- webdesign/js/theseus.js
+++ webdesign/js/theseus.js
@@ -7,9 +7,18 @@ $(document).ready(function() {
     var code;
     $('#submit').click(function() {
         code = ($('#book-code').val());
-        smoothScroll.animateScroll('#map');
 
-        // put an image
+        if (code == 'E 743.5 .H43 2011') {
+            $("#book-floor").attr("src","img/floors/hesburgh10.jpg");
+            $('#book-title').text('What\'s Fair On The Air?');
+            $('#book-author').text('Heather Hendershot');
+        }
+        else if (code == 'E 208 .M85 2012') {
+            $("#book-floor").attr("src","img/floors/hesburgh10.jpg");
+            $('#book-title').text('The Birth of the Republic');
+            $('#book-author').text('Edmund S. Morgan');
+        }
+        smoothScroll.animateScroll('#map');
 
         return false;
     });
