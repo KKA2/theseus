84d04dad245b885abd0eeaf785cae509133156a4
Katricia Herring <kherring@nd.edu>
2016-03-19 13:54:52 -0400

    takes input and creates array

---
+++ convertToPix.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#converts inputting Library of Congress classification to a location the shelf
+#Authors: Kat Herring, Kate Barlock, Ann Keenan
+#Date: 3/19/16
+
+while getopts h name
+do
+	case $name in 	
+		h) echo "usage: convertToPix.sh 'LCC call number' 
+Converts book number to its appropriate pixel coordinates on the correct floor map
+
+  -h : details usage";;	
+		*) echo "Invalid arg" 
+			exit 1;;
+			
+	esac
+done
+
+shift $(($OPTIND -1))
+
+i=0
+for section in $1
+do
+	array[i]="$section"
+	i=$((i + 1))
+done
+
+#NOTE: $0 gives subject (ex. PS); $1 is first set of numbers
+echo ${array[0]}
